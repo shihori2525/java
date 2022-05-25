f4c492c73b2f71ae8b965e871fdef6c36dad6b5d
shihori2525 <rirakkuma.250@gmail.com>
2022-05-25 23:25:03 +0900

    [Finish]chapter4

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		String str = "sakata";
+		System.out.println(str);
+
+		str = "ice";
+		System.out.println(str);
+
+		final int lucky_number = 1;
+		System.out.println(lucky_number);
+
+	}
+
+}
