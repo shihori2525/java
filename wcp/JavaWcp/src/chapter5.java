e89dfa4362c1a2e47bc11c47f99abce1627f071f
shihori2525 <rirakkuma.250@gmail.com>
2022-05-27 21:31:34 +0900

    [Start]chapter5

---
+++ wcp/JavaWcp/src/chapter5.java
@@ -0,0 +1,14 @@
+
+public class chapter5 {
+	public static void main(String[] args) {
+		long l = 10000000000000L;
+		System.out.println(l);
+		
+		double d = 3.14;
+		System.out.println(d);
+		
+		boolean b = true;
+		System.out.println(b);
+	}
+
+}
