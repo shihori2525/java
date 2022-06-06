1dc28e30f7502eb78e004bf10d309a97c860caae
shihori2525 <rirakkuma.250@gmail.com>
2022-06-06 20:15:46 +0900

    [Add]operator

--- wcp/JavaWcp/src/Variable09.java
+++ wcp/JavaWcp/src/Variable09.java
@@ -1,7 +1,13 @@
 
 public class Variable09 {
 	String a = "applePie";
-	String b  = "applePie";
+	String b = "applePie";
 	c = a == b;
+	
+	
+	String a = "applePie";
+	String b = "apple";
+	String e = d + "Pie";
+	c = a == e;
 
 }
