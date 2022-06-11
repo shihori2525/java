1f2cffdb25fcb11693530ed732790bdc9b6f1717
shihori2525 <rirakkuma.250@gmail.com>
2022-06-11 17:39:08 +0900

    [Add]if02

--- wcp/JavaWcp/src/if02.java
+++ wcp/JavaWcp/src/if02.java
@@ -4,6 +4,12 @@ public class if02 {
 		int number = 7;
 		if(number < 5) {
 			System.out.println(number + "は５未満です");
+		}else if (number < 10) {
+			System.out.println(number + "は５以上、１０未満です");
+		}else if (number < 15) {
+			System.out.println(number + "は１０以上、１５未満です");
+		}else {
+			System.out.println(number + "は15未満ではないです");
 		}
 	}
 
