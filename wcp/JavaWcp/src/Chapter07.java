64b92249f9a6295d498f8393a97da7e3636d8189
shihori2525 <rirakkuma.250@gmail.com>
2022-06-20 22:59:49 +0900

    [Add]chapter07

--- wcp/JavaWcp/src/Chapter07.java
+++ wcp/JavaWcp/src/Chapter07.java
@@ -6,6 +6,8 @@ public class Chapter07 {
 			System.out.println("とても近いです");
 		}else if (distance <= 10) {
 			System.out.println("近いです");
+		}else if (distance <= 15){
+			System.out.println("遠いです");
 		}
 	}
 
