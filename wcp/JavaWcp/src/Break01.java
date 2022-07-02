baa616a612739e87a782d8670d1484ba8181e651
shihori2525 <rirakkuma.250@gmail.com>
2022-07-02 21:54:35 +0900

    [Add]break

--- wcp/JavaWcp/src/Break01.java
+++ wcp/JavaWcp/src/Break01.java
@@ -2,7 +2,10 @@
 public class Break01 {
 	public static void main(String[] args) {
 		for (int count = 0; count < 5; count++) {
-			if (count == 2)
+			if (count == 2) {
+				break;
+			}
+			System.out.println("Break01 = " + count);
 		}
 	}
 
