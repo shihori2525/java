f0d1149381eb013af6cd29f64fcf2903cf38f7fb
shihori2525 <rirakkuma.250@gmail.com>
2022-07-06 21:35:51 +0900

    [Add]recursive01

--- wcp/JavaWcp/src/Recursive01.java
+++ wcp/JavaWcp/src/Recursive01.java
@@ -4,5 +4,14 @@ public class Recursive01 {
 		int number = 1;
 		printNum(number);
 	}
+	
+	public static int printNum(int number) {
+		if (number <50) {
+			number *2 ;
+			System.out.println("While01 =" + number);
+			printNum(number);
+		}
+		return number;
+	}
 
 }
