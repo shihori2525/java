0f5d301e55c8a551bcf554d3dc2ad0b81c395712
shihori2525 <rirakkuma.250@gmail.com>
2022-07-18 21:42:45 +0900

    [Add]no return

--- wcp/JavaWcp/src/classMethod/Human03.java
+++ wcp/JavaWcp/src/classMethod/Human03.java
@@ -14,5 +14,14 @@ public class Human03 {
 	public String getProfile() {
 		return "年齢は" + this.age + "、職業は" + this.profession;
 	}
+	
+	public void greet(String friend) {
+		if(friend == null) {
+			System.out.println("挨拶する友達がわかりません！");
+			return;
+		}
+		System.out.println(friend + "さん、こんにちは");
+		
+	}
 
 }
