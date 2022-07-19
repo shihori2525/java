861c2a8a551a11244522abfb06a97b661fc190fc
shihori2525 <rirakkuma.250@gmail.com>
2022-07-19 20:40:21 +0900

    [Start]class method 03

---
+++ wcp/JavaWcp/src/classMethod/ClassMethod03.java
@@ -0,0 +1,12 @@
+package classMethod;
+
+public class ClassMethod03 {
+	public static void main(String[] args) {
+		Human03 yamada = new Human03();
+		
+		System.out.println("私の名前は、" + yamada.neme + "です。");
+		String prifile = yamada.getProfile();
+		System.out.println(profile + "です。");
+	}
+
+}
